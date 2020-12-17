c05fac2e6b0100d911ccfcd4bcb7ae290eba3f80
tengu <aiki_tengu@yahoo.com>
2020-12-17 11:14:29 +0200

    working with themovieDB API

--- src/main/java/io/javabrains/movieinfoservice/models/Movie.java
+++ src/main/java/io/javabrains/movieinfoservice/models/Movie.java
@@ -1,9 +1,17 @@
-package io.javabrains.movieinfoservice.model;
+package io.javabrains.movieinfoservice.models;
 
 public class Movie {
 
     private String movieId;
     private String name;
+    private String description;
+
+
+    public Movie(String movieId, String name, String description) {
+        this.movieId = movieId;
+        this.name = name;
+        this.description = description;
+    }
 
     public String getMovieId() {
         return movieId;
@@ -24,5 +32,13 @@ public class Movie {
     public Movie(String movieId, String name) {
         this.movieId = movieId;
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
